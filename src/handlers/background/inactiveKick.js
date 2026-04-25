a81bb7c9ec5464aba6076d77bc57fee394131e9f
manilawestern-debug <manilawestern@gmail.com>
2026-04-25 11:20:03 +0800

    Add inactiveKick handler to remove inactive users
    Implement inactive user kick functionality based on last active timestamp.

--- src/handlers/background/inactiveKick.js
+++ src/handlers/background/inactiveKick.js
@@ -1,1 +1,36 @@
+"use strict";
 
+module.exports.config = {
+    name: "inactiveKick",
+    interval: 60 * 60 * 1000
+};
+
+module.exports.execute = async function ({ api, db }) {
+    try {
+        if (!db) return;
+
+        const now = Date.now();
+        const limit = 3 * 24 * 60 * 60 * 1000;
+
+        const users = await db.collection("activity").find({}).toArray();
+
+        for (const user of users) {
+            if (now - user.lastActive > limit) {
+                try {
+                    await api.gcmember("remove", user.userID, user.threadID);
+
+                    await db.collection("activity").deleteOne({
+                        userID: user.userID,
+                        threadID: user.threadID
+                    });
+
+                } catch (err) {
+                    console.log(err.message);
+                }
+            }
+        }
+
+    } catch (err) {
+        console.error(err);
+    }
+};
